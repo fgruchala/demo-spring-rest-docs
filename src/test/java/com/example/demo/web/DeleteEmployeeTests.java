3324d8d09ceab406928ee18665f738d8490de750
François GRUCHALA <@FGruchala>
2017-06-27 13:25:26 +0200

    Update Tests

--- src/test/java/com/example/demo/web/DeleteEmployeeTests.java
+++ src/test/java/com/example/demo/web/DeleteEmployeeTests.java
@@ -6,13 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.restdocs.payload.PayloadDocumentation;
-import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
@@ -28,17 +24,14 @@ public class DeleteEmployeeTests extends AbstractEmployeeWebService {
 
         // WHEN
         ResultActions resultActions = mockMvc.perform(
-                RestDocumentationRequestBuilders
+                MockMvcRequestBuilders
                         .delete(URL + "/{id}", employee1.getId())
                         .accept(MediaType.APPLICATION_JSON_UTF8));
 
         // THEN
         resultActions
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(""))
-                .andDo(MockMvcRestDocumentation.document(
-                        "{class-name}/{method-name}",
-                        RequestDocumentation.pathParameters(parameterDescriptorsEmployeeId)));
+                .andExpect(MockMvcResultMatchers.content().string(""));
 
     }
 
@@ -54,19 +47,14 @@ public class DeleteEmployeeTests extends AbstractEmployeeWebService {
 
         // WHEN
         ResultActions resultActions = mockMvc.perform(
-                RestDocumentationRequestBuilders
+                MockMvcRequestBuilders
                         .delete(URL + "/{id}", id)
                         .accept(MediaType.APPLICATION_JSON_UTF8));
 
         // THEN
         resultActions
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.error", Matchers.is("Employee (ID 99) not found.")))
-                .andDo(MockMvcRestDocumentation.document(
-                        "{class-name}/{method-name}",
-                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
-                        RequestDocumentation.pathParameters(parameterDescriptorsEmployeeId),
-                        PayloadDocumentation.responseFields(fieldDescriptorsSimpleError)));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.error", Matchers.is("Employee (ID 99) not found.")));
     }
 
 }

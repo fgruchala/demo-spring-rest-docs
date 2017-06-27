03a5773ca5d9c25a473cfd7d45b560b2d874c550
François GRUCHALA <@FGruchala>
2017-06-27 13:48:01 +0200

    Update Tests

--- src/test/java/com/example/demo/web/AbstractEmployeeWebService.java
+++ src/test/java/com/example/demo/web/AbstractEmployeeWebService.java
@@ -7,17 +7,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.payload.FieldDescriptor;
-import org.springframework.restdocs.payload.PayloadDocumentation;
-import org.springframework.restdocs.request.ParameterDescriptor;
-import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(com.example.demo.webservice.v1.EmployeeWebService.class)
-@AutoConfigureRestDocs("target/generated-snippets")
 public abstract class AbstractEmployeeWebService {
 
     @Autowired

6d5bb765c21515e5383ac106e407e3aeb36d2782
François GRUCHALA <@FGruchala>
2017-06-27 13:49:57 +0200

    Update Tests

--- src/test/java/com/example/demo/web/AbstractEmployeeWebService.java
+++ src/test/java/com/example/demo/web/AbstractEmployeeWebService.java
@@ -10,10 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.payload.FieldDescriptor;
-import org.springframework.restdocs.payload.PayloadDocumentation;
-import org.springframework.restdocs.request.ParameterDescriptor;
-import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(com.example.demo.webservice.v1.EmployeeWebService.class)
